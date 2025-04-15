769b656a56d563a22ff1cca0e78fa6ba527fa3e0
Sanju-shreee <sanjusri2022@gmail.com>
2025-04-14 20:01:47 -0400

    Add files via upload

--- MAIN.py
+++ MAIN.py
@@ -11,7 +11,23 @@ from claw import open_claw, close_claw, cleanup
 from time import sleep
 import iamhome
 
-# Prompt user to place droplet on surface. Loop until the user presses ENTER without typing anything
+microstepping_setting = "1/32"
+stepfactor = 32
+'''
+if microstepping_setting == "1/32":
+    stepfactor = 32
+elif microstepping_setting == "1/16":
+    stepfactor == 16
+elif microstepping_setting == "1/8":
+    stepfactor == 8
+elif microstepping_setting == "1/4":
+    stepfactor == 4
+elif microstepping_setting == "1/2":
+    stepfactor == 2
+else:
+    stepfactor == 1
+'''
+#Prompt user to place droplet on surface. Loop until the user presses ENTER without typing anything
 while True:
     user_input = input("Please press ENTER after you successfully place a droplet on the surface (within frame)!")
 
@@ -31,9 +47,27 @@ def get_integers(prompt):
         except ValueError:
             print("YIKES! Error: Please enter a valid integer.")
 
+Max_X_steps = 15400
+Max_Y_steps = 16000
+conv_mmtosteps = 4.7619
+
 # End Coordinates the droplet must be transported to in units of mm            
-EndPointX = get_integers('Enter desired X coordinate (mm): ')
-EndPointY = get_integers('Enter desired Y coordinate (mm): ')
+while True:
+    EndPointX = get_integers('Enter desired X coordinate between 0-100 (mm): ')
+    EndPoint_Xsteps = int(EndPointX*conv_mmtosteps*32)
+    if 0 <= EndPoint_Xsteps <= Max_X_steps:
+        break
+    else:
+        print(f"Please enter a value between 0 and {int(Max_X_steps/(32*conv_mmtosteps))}.")
+        
+while True:
+    EndPointY = get_integers('Enter desired Y coordinate between 0-105 (mm): ')
+    EndPoint_Ysteps = int(EndPointY*conv_mmtosteps*32)
+    if 0 <= EndPoint_Ysteps <= Max_Y_steps:
+        break
+    else:
+        print(f"Please enter a value between 0 and {int(Max_Y_steps/(32*conv_mmtosteps))}.")
+
 EndPoint = np.array([EndPointX, EndPointY])
 # Prints end coordinates
 print(EndPoint)
@@ -63,17 +97,15 @@ claw_origin = (30.8, 49.8) #(y,x)
 # Distance from droplet's origin to the claw origin in units of mm?
 claw_drop = (drop_origin[0] - claw_origin[0], drop_origin[1] - claw_origin[1])
 
-conv_mmtosteps = 4.7619
-
 # Distance from droplet's origin to the claw in terms of steps
 x_steps = int(claw_drop[1]*conv_mmtosteps)
 y_steps = int(claw_drop[0]*conv_mmtosteps)
 
 # Moving the Y axis to droplet's initial y position
-stepper_y.move_motor(y_steps, 0, 0.002, "Full")
+stepper_y.move_motor(y_steps*stepfactor, 0, 0.0002/16, microstepping_setting)
 
 # Moving the X axis to droplet's initial x position
-stepper_x.move_motor(x_steps, 1, 0.002, "Full")
+stepper_x.move_motor(x_steps*stepfactor, 1, 0.0002/16, microstepping_setting)
 
 # Open Claw: Servo motor rotates (to 85 degrees)
 # Specifiy step delay for the servo motor to adjust speed of claw
@@ -84,7 +116,7 @@ sleep(1)
 
 # Z axis moves down to surface.
 # The droplet should be in between the left and right claws after the Z axis has come down. If not, camera calibration (mechanical) might be off.
-stepper_z.move_motor(940, 0, 0.002, "Full")
+stepper_z.move_motor(915*stepfactor, 0, 0.00002/16, microstepping_setting)
 
 sleep(1)
 
@@ -94,7 +126,7 @@ claw.close_claw(step_delay)
 sleep(3)
 
 # Z-axis moves up for clearance
-stepper_z.move_motor(300, 1, 0.002, "Full")
+stepper_z.move_motor(300*stepfactor, 1, 0.00002/16, microstepping_setting)
 
 # Convert the droplet's end coordinates from mm to steps
 EndPoint_Xsteps = int(EndPointX*conv_mmtosteps)
@@ -106,22 +138,24 @@ Y_final = y_steps - EndPoint_Ysteps
 
 # Conditional Movement: X
 if X_final > 0:
-    stepper_x.move_motor(X_final, 0, 0.002, "Full")
+    stepper_x.move_motor(X_final*stepfactor, 0, 0.0002/16, microstepping_setting)
     print("X moving in -x direction")
 else:
-    stepper_x.move_motor(X_final, 1, 0.002, "Full")
+    X_final = abs(X_final)
+    stepper_x.move_motor(X_final*stepfactor, 1, 0.0002/16, microstepping_setting)
     print("X moving in +x direction")
 
 # Conditional Movement: Y
 if Y_final > 0:
-    stepper_y.move_motor(Y_final, 1, 0.002, "Full")
+    stepper_y.move_motor(Y_final*stepfactor, 1, 0.0002/16, microstepping_setting)
     print("X moving in -y direction")
 else:
-    stepper_y.move_motor(Y_final, 0, 0.002, "Full")
+    Y_final = abs(Y_final)
+    stepper_y.move_motor(Y_final*stepfactor, 0, 0.0002/16, microstepping_setting)
     print("X moving in +y direction")
 
 # Z-axis moves down
-stepper_z.move_motor(300, 0, 0.002, "Full")
+stepper_z.move_motor(300*stepfactor, 0, 0.00002/16, microstepping_setting)
 
 # Claw opens
 #claw.servo_angle(85)
@@ -131,26 +165,21 @@ print("Droplet has been placed!")
 sleep(2)
 
 # Z up
-stepper_z.move_motor(640, 1, 0.002, "Full")
+stepper_z.move_motor(615*stepfactor, 1, 0.00002/16, microstepping_setting)
 
 # close claw
 #claw.servo_angle(68)
 claw.close_claw(step_delay)
 
-sleep(2)
-
 # Home
-#taking X home
-stepper_y.move_motor(40, 0, 0.002, "Full")# Y Limit Switch Clearance
-stepper_x.move_motor(EndPoint_Xsteps, 0, 0.002, "Full")
-iamhome.home_axis("X", stepper_x, 1, .5, 0)
-#taking Y home
-stepper_y.move_motor(40, 1, 0.002, "Full")# Undo Y Limit Switch Clearance
-stepper_y.move_motor(EndPoint_Ysteps, 1, 0.002, "Full")
-iamhome.home_axis("Y", stepper_y, 1, .5, 0)
-#taking Z home
-iamhome.home_axis("Z", stepper_z, 10, .02, 1)
-
+if EndPoint_Ysteps*stepfactor > 40*stepfactor:
+    print("Not moving to clear Y Limit Switch")
+    iamhome.home_axis("X", stepper_x, 1, 0.0002/16, 0) #homing x
+    iamhome.home_axis("Y", stepper_y, 1, 0.0002/16, 1) #homing y
+    iamhome.home_axis("Z", stepper_z, 1, 0.00002/16, 1) #homing z
+else:
+    print("Moving to clear Y Limit Switch")
+    iamhome.homing_sequence()
 
 # Cleanup GPIO
 XYZ_Limits_Approach_Infinity.cleanup()
@@ -158,3 +187,5 @@ stepper_x.cleanup()
 stepper_y.cleanup()
 stepper_z.cleanup()
 claw.cleanup()
+
+    
