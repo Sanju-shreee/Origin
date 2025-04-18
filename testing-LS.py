57bcaf86d007adcf5f2c07c4a91d2f9706ca3a79
Sanju-shreee <sanjusri2022@gmail.com>
2025-04-17 21:11:19 -0400

    Create testing-LS.py

---
+++ testing-LS.py
@@ -0,0 +1,6 @@
+import XYZ_Limits_Approach_Infinity
+
+while True:
+    #print(f"X Limit Switch: {XYZ_Limits_Approach_Infinity.is_limit_pressed('X')}")
+    #print(f"Y Limit Switch: {XYZ_Limits_Approach_Infinity.is_limit_pressed('Y')}")   
+    print(f"Z Limit Switch: {XYZ_Limits_Approach_Infinity.is_limit_pressed('Z')}")
