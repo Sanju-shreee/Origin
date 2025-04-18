f3f8f1ee32b76cf723a9ecdc332632952152202c
Sanju-shreee <sanjusri2022@gmail.com>
2025-04-17 21:02:14 -0400

    Update iamhome.py

--- iamhome.py
+++ iamhome.py
@@ -5,8 +5,11 @@ import stepper_z
 import XYZ_Limits_Approach_Infinity
 from time import sleep
 
+microstepping_setting = "1/32"
+stepfactor = 32
+
 # HOMING SEQUENCE: Move towards limit switches until pressed
-def home_axis(axis, stepper_module, steps=1, delay=0.5, direction=1):
+def home_axis(axis, stepper_module, steps=1, delay=0.002/16, direction=1):
     '''
     Moves the given axis towards its limit switch until it is pressed.
     :param axis: "X", "Y", or "Z"
@@ -17,19 +20,20 @@ def home_axis(axis, stepper_module, steps=1, delay=0.5, direction=1):
     '''
     print(f"Homing {axis}-axis...")
     while not XYZ_Limits_Approach_Infinity.is_limit_pressed(axis):
-        print(f"{axis}-axis moving... Limit switch state: {XYZ_Limits_Approach_Infinity.is_limit_pressed(axis)}")
-        stepper_module.move_motor(steps, direction, delay, "Full")
+        #print(f"{axis}-axis moving... Limit switch state: {XYZ_Limits_Approach_Infinity.is_limit_pressed(axis)}")
+        stepper_module.move_motor(steps*stepfactor, direction, delay, microstepping_setting)
     
     print(f"{axis}-axis homed successfully!")
 
 def homing_sequence():
     #Move Y and X to clear limit switches
-    stepper_y.move_motor(40, 0, 0.002, "Full")
-    stepper_z.move_motor(300, 0, 0.002, "Full")
+    stepper_y.move_motor(40*stepfactor, 0, 0.0001/16, microstepping_setting)
+    stepper_z.move_motor(360*stepfactor, 0, 0.00002/16, microstepping_setting)
 
     # Perform homing for each axis
-    home_axis("X", stepper_x, 1, .5, 0)
-    stepper_y.move_motor(40, 1, 0.002, "Full") #Undo Y Limit Switch Clearance
-    home_axis("Y", stepper_y, 1, .5, 1)
-    stepper_z.move_motor(300, 1, 0.002, "Full") #Undo X Limit Switch Clearance
-    home_axis("Z", stepper_z, 10, .02, 1)
+    home_axis("X", stepper_x, 1, 0.0001/16, 0)
+    stepper_y.move_motor(40*stepfactor, 1, 0.0001/16, microstepping_setting) #Undo Y Limit Switch Clearance
+    home_axis("Y", stepper_y, 1, 0.0001/16, 1)
+    stepper_z.move_motor(360*stepfactor, 1, 0.00002/16, microstepping_setting) #Undo Z Limit Switch Clearance
+    home_axis("Z", stepper_z, 1, .00002/16, 1)
+
