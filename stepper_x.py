1e2355819afb48771bf34b20d0faf6f4ede9be84
Sanju-shreee <sanjusri2022@gmail.com>
2025-04-17 21:04:08 -0400

    Update stepper_x.py

--- stepper_x.py
+++ stepper_x.py
@@ -5,6 +5,8 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
+microstepping_setting = "1/32"
+stepfactor = 32
 
 # Define the GPIO Pins
 DIR_pin = 16 # Direction pin
@@ -26,7 +28,7 @@ GPIO.setup(M2, GPIO.OUT)
 
 #Microstepping settings: (Change these to set to different modes):
 # Options: "Full", "1/2", "1/4", "1/8", "1/16", "1/32"
-Microstep_mode = "Full"
+Microstep_mode = microstepping_setting
 
 # Reference that maps modes to M0, M1, and M2 settings:
 Microstep_settings = {
@@ -69,3 +71,4 @@ def cleanup():
         # Need to get RPiMotorLib to work soon:
 # import rpimotorlib
 # print("RPiMotorLib installed successfully!")
+
